4619c2a00cc910217260a566f4fa86f7fd9aefca
Patrick McDaniel <patrick.mcdaniel@ascension-external.org>
2023-03-13 15:55:27 -0500

    parse address parts

--- str-split.js
+++ str-split.js
@@ -1,15 +1,90 @@
 // node strSplit.js
+let address = " 3808 Main Street, 210, Austin, TX  12345";
 
-let fullName = "Jerry Jones";
+const addressObj = {
+  num: "",
+  name: "",
+  apt: "",
+  city: "",
+  state: "",
+  zip: "",
+};
 
-function splitName() {
-  let strArray = fullName.split(/(\s+)/);
-  console.log("strArray: ", strArray);
-  console.log("strArray.lengtth: ", strArray.length);
-  let fName = strArray[0].length > 0 ? strArray[0] : "";
-  let lName = strArray[2].length > 0 ? strArray[2] : "";
-  console.log("fname: ", fName);
-  console.log("lname: ", lName);
+function getStreetNumber() {
+  console.log("address: ", address);
+  if (address !== "") {
+    let strArray = address.split(",");
+
+    const streetNum = strArray[0].match(/(\d+)/);
+    if (streetNum !== null) {
+      addressObj.name = address.substring(
+        streetNum[0].length + 1,
+        address.length
+      );
+      addressObj.num = streetNum[0].split("");
+    } else {
+      addressObj.name = address;
+    }
+    return addressObj.num;
+  }
 }
 
-splitName();
+function getStreetName() {
+  var streetName = addressObj.name;
+  if (streetName !== "") {
+    let strArray = streetName.split(",");
+    streetName = strArray[0];
+    streetName = streetName.trim();
+  }
+  return streetName;
+}
+
+function splitApartment() {
+  var apt = addressObj.name;
+  let strArray = apt.split(",");
+  if (strArray.length >= 4) {
+    return strArray[1];
+  }
+  return "";
+}
+
+function getCity() {
+  let strArray = address.split(",");
+  if (apt !== "") {
+    return strArray[2].trim();
+  } else {
+    return strArray[1].trim();
+  }
+}
+
+function getState() {
+  let strArray = address.split(",");
+  if (apt !== "") {
+    return strArray[3].trim().slice(0, 2);
+  } else {
+    return strArray[2].trim().slice(0, 2);
+  }
+}
+
+function getZip() {
+  let strArray = address.split(/(\s+)/);
+  return strArray[strArray.length - 1];
+}
+
+const streetNum = getStreetNumber();
+console.log("streetNum:", streetNum);
+
+const streetName = getStreetName();
+console.log("streetName:", streetName);
+
+const apt = splitApartment();
+console.log("apt:", apt);
+
+const city = getCity();
+console.log("city:", city);
+
+const state = getState();
+console.log("state:", state);
+
+const zip = getZip();
+console.log("zip:", zip);
